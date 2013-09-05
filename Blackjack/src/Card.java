59b44291423ee29b4f3cbad21ee3459d4f9763f5
RigelOrionBeta <rigelorionbeta@gmail.com>
2013-09-04 20:35:19 -0400

    Added values.

--- Blackjack/src/Card.java
+++ Blackjack/src/Card.java
@@ -3,14 +3,60 @@ public class Card {
 	// this card's attributes
 	private Suit suit;
 	private Rank rank;
+	private int value;
 	
 	public Card(Rank rank, Suit suit) {
 		this.rank = rank;
 		this.suit = suit;
+		switch(rank) {
+		case Ace:
+			value = 1;
+			break;
+		case Two:
+			value = 2;
+			break;
+		case Three:
+			value = 3;
+			break;
+		case Four:
+			value = 4;
+			break;
+		case Five:
+			value = 5;
+			break;
+		case Six:
+			value = 6;
+			break;
+		case Seven:
+			value = 7;
+			break;
+		case Eight:
+			value = 8;
+			break;
+		case Nine:
+			value = 9;
+			break;
+		default:
+			value = 10;
+			break;
+		}
+		
+	}
+	
+	public Rank getRank() { 
+		return rank;
+	}
+	
+	public Suit getSuit() { 
+		return suit;
+	}
+	
+	public int getValue() {
+		return value;
 	}
 	
 	public String toString() {
-		return rank + " of " + suit + "s";
+		return rank + " of " + suit + "s, value " + value;
 	}
 
 }
--- Blackjack/src/Deck.java
+++ Blackjack/src/Deck.java
@@ -4,6 +4,9 @@ public class Deck {
 
 	ArrayList<Card> deck = new ArrayList<Card>();
 	
+	/* Constructor
+	 * 	Creates an unshuffled deck of cards with all combinations of suits and ranks
+	 */
 	public Deck() {
 		Rank[] ranks = Rank.values();
 		Suit[] suits = Suit.values();
@@ -14,6 +17,30 @@ public class Deck {
 		}
 	}
 	
+	public void shuffle() {
+		
+	}
+	
+	/* draw()
+	 *  returns top card and removes it from deck
+	 */
+	public Card draw() {
+		Card card = deck.get(0);
+		deck.remove(0);
+		return card;
+	}
+	
+	/* size()
+	 *  returns current size of the deck
+	 */
+	public int size() {
+		return deck.size();
+	}
+	
+	/* toString()
+	 * 
+	 * return - returns a string representing what's in the deck currently.
+	 */
 	public String toString() {
 		String returnval = "";
 		for(int i = 0; i < deck.size(); i++) {
